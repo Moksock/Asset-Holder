ccb8dc366a2953b6c1281210b5d14d2eb109897b
Moksock <moksockproductions@gmail.com>
2026-07-24 21:06:08 -0400

    Pixel Grit Added
    Added Pixel Grit

---
+++ Mana Collection/Mana Seed Tools (1.12)/Mana Seed Tools/process_all_textures.bat
@@ -0,0 +1,342 @@
+@ECHO OFF
+
+packer.exe demn_v01
+packer.exe demn_v02
+
+packer.exe gbln_v01
+
+packer.exe humn_v00
+packer.exe humn_v01
+packer.exe humn_v02
+packer.exe humn_v03
+packer.exe humn_v04
+packer.exe humn_v05
+packer.exe humn_v06
+packer.exe humn_v07
+packer.exe humn_v08
+packer.exe humn_v09
+packer.exe humn_v10
+
+packer.exe boxr_v01 1out/
+
+packer.exe undi_v01 1out/
+
+packer.exe bob1_v00 4har/
+packer.exe bob1_v01 4har/
+packer.exe bob1_v02 4har/
+packer.exe bob1_v03 4har/
+packer.exe bob1_v04 4har/
+packer.exe bob1_v05 4har/
+packer.exe bob1_v06 4har/
+packer.exe bob1_v07 4har/
+packer.exe bob1_v08 4har/
+packer.exe bob1_v09 4har/
+packer.exe bob1_v10 4har/
+packer.exe bob1_v11 4har/
+packer.exe bob1_v12 4har/
+packer.exe bob1_v13 4har/
+
+packer.exe bob2_v00 4har/
+packer.exe bob2_v01 4har/
+packer.exe bob2_v02 4har/
+packer.exe bob2_v03 4har/
+packer.exe bob2_v04 4har/
+packer.exe bob2_v05 4har/
+packer.exe bob2_v06 4har/
+packer.exe bob2_v07 4har/
+packer.exe bob2_v08 4har/
+packer.exe bob2_v09 4har/
+packer.exe bob2_v10 4har/
+packer.exe bob2_v11 4har/
+packer.exe bob2_v12 4har/
+packer.exe bob2_v13 4har/
+
+packer.exe dap1_v00 4har/
+packer.exe dap1_v01 4har/
+packer.exe dap1_v02 4har/
+packer.exe dap1_v03 4har/
+packer.exe dap1_v04 4har/
+packer.exe dap1_v05 4har/
+packer.exe dap1_v06 4har/
+packer.exe dap1_v07 4har/
+packer.exe dap1_v08 4har/
+packer.exe dap1_v09 4har/
+packer.exe dap1_v10 4har/
+packer.exe dap1_v11 4har/
+packer.exe dap1_v12 4har/
+packer.exe dap1_v13 4har/
+
+packer.exe flat_v00 4har/
+packer.exe flat_v01 4har/
+packer.exe flat_v02 4har/
+packer.exe flat_v03 4har/
+packer.exe flat_v04 4har/
+packer.exe flat_v05 4har/
+packer.exe flat_v06 4har/
+packer.exe flat_v07 4har/
+packer.exe flat_v08 4har/
+packer.exe flat_v09 4har/
+packer.exe flat_v10 4har/
+packer.exe flat_v11 4har/
+packer.exe flat_v12 4har/
+packer.exe flat_v13 4har/
+
+packer.exe fro1_v00 4har/
+packer.exe fro1_v01 4har/
+packer.exe fro1_v02 4har/
+packer.exe fro1_v03 4har/
+packer.exe fro1_v04 4har/
+packer.exe fro1_v05 4har/
+packer.exe fro1_v06 4har/
+packer.exe fro1_v07 4har/
+packer.exe fro1_v08 4har/
+packer.exe fro1_v09 4har/
+packer.exe fro1_v10 4har/
+packer.exe fro1_v11 4har/
+packer.exe fro1_v12 4har/
+packer.exe fro1_v13 4har/
+
+packer.exe pon1_v00 4har/
+packer.exe pon1_v01 4har/
+packer.exe pon1_v02 4har/
+packer.exe pon1_v03 4har/
+packer.exe pon1_v04 4har/
+packer.exe pon1_v05 4har/
+packer.exe pon1_v06 4har/
+packer.exe pon1_v07 4har/
+packer.exe pon1_v08 4har/
+packer.exe pon1_v09 4har/
+packer.exe pon1_v10 4har/
+packer.exe pon1_v11a 4har/
+packer.exe pon1_v11b 4har/
+packer.exe pon1_v12 4har/
+packer.exe pon1_v13 4har/
+
+packer.exe spk2_v00 4har/
+packer.exe spk2_v01 4har/
+packer.exe spk2_v02 4har/
+packer.exe spk2_v03 4har/
+packer.exe spk2_v04 4har/
+packer.exe spk2_v05 4har/
+packer.exe spk2_v06 4har/
+packer.exe spk2_v07 4har/
+packer.exe spk2_v08 4har/
+packer.exe spk2_v09 4har/
+packer.exe spk2_v10 4har/
+packer.exe spk2_v11 4har/
+packer.exe spk2_v12 4har/
+packer.exe spk2_v13 4har/
+
+packer.exe farm_v01 6tla/ 1
+packer.exe mine_v01 6tla/ 2
+packer.exe wood_v01 6tla/ 3
+packer.exe roda_v01 6tla/ 4
+packer.exe smth_v01 6tla/ 5
+
+packer.exe bo01_v01 6tla/ 6
+packer.exe bo01_v02 6tla/ 6
+packer.exe bo01_v03 6tla/ 6
+packer.exe bo01_v04 6tla/ 6
+packer.exe bo01_v05 6tla/ 6
+packer.exe bo02_v01 6tla/ 6
+packer.exe bo02_v02 6tla/ 6
+packer.exe bo02_v03 6tla/ 6
+packer.exe bo02_v04 6tla/ 6
+packer.exe bo02_v05 6tla/ 6
+packer.exe bo03_v01 6tla/ 6
+packer.exe bo03_v02 6tla/ 6
+packer.exe bo03_v03 6tla/ 6
+packer.exe bo03_v04 6tla/ 6
+packer.exe bo03_v05 6tla/ 6
+
+packer.exe qv01_v01 7tlb/ 6
+packer.exe qv01_v02 7tlb/ 6
+packer.exe qv01_v03 7tlb/ 6
+packer.exe qv01_v04 7tlb/ 6
+packer.exe qv01_v05 7tlb/ 6
+packer.exe qv01_v06 7tlb/ 6
+packer.exe qv01_v07 7tlb/ 6
+packer.exe qv01_v08 7tlb/ 6
+
+packer.exe ax01_v01 6tla/ 7
+packer.exe ax01_v02 6tla/ 7
+packer.exe ax01_v03 6tla/ 7
+packer.exe ax01_v04 6tla/ 7
+packer.exe ax01_v05 6tla/ 7
+packer.exe mc01_v01 6tla/ 7
+packer.exe mc01_v02 6tla/ 7
+packer.exe mc01_v03 6tla/ 7
+packer.exe mc01_v04 6tla/ 7
+packer.exe mc01_v05 6tla/ 7
+packer.exe sw01_v01 6tla/ 7
+packer.exe sw01_v02 6tla/ 7
+packer.exe sw01_v03 6tla/ 7
+packer.exe sw01_v04 6tla/ 7
+packer.exe sw01_v05 6tla/ 7
+packer.exe sw02_v01 6tla/ 7
+packer.exe sw02_v02 6tla/ 7
+packer.exe sw02_v03 6tla/ 7
+packer.exe sw02_v04 6tla/ 7
+packer.exe sw02_v05 6tla/ 7
+
+packer.exe sh01_v01 7tlb/ 7
+packer.exe sh01_v02 7tlb/ 7
+packer.exe sh01_v03 7tlb/ 7
+packer.exe sh01_v04 7tlb/ 7
+packer.exe sh01_v05 7tlb/ 7
+packer.exe sh02_v01 7tlb/ 7
+packer.exe sh02_v02 7tlb/ 7
+packer.exe sh02_v03 7tlb/ 7
+packer.exe sh02_v04 7tlb/ 7
+packer.exe sh02_v05 7tlb/ 7
+packer.exe sh03_v01 7tlb/ 7
+packer.exe sh03_v02 7tlb/ 7
+packer.exe sh03_v03 7tlb/ 7
+packer.exe sh03_v04 7tlb/ 7
+packer.exe sh03_v05 7tlb/ 7
+
+packer.exe hb01_v01 6tla/ 8
+packer.exe hb01_v02 6tla/ 8
+packer.exe hb01_v03 6tla/ 8
+packer.exe hb01_v04 6tla/ 8
+packer.exe hb01_v05 6tla/ 8
+packer.exe sp01_v01 6tla/ 8
+packer.exe sp01_v02 6tla/ 8
+packer.exe sp01_v03 6tla/ 8
+packer.exe sp01_v04 6tla/ 8
+packer.exe sp01_v05 6tla/ 8
+packer.exe sp02_v01 6tla/ 8
+packer.exe sp02_v02 6tla/ 8
+packer.exe sp02_v03 6tla/ 8
+packer.exe sp02_v04 6tla/ 8
+packer.exe sp02_v05 6tla/ 8
+
+packer.exe alch_v01 1out/
+packer.exe alch_v02 1out/
+packer.exe alch_v03 1out/
+packer.exe alch_v04 1out/
+packer.exe alch_v05 1out/
+
+packer.exe angl_v01 1out/
+packer.exe angl_v02 1out/
+packer.exe angl_v03 1out/
+packer.exe angl_v04 1out/
+packer.exe angl_v05 1out/
+
+packer.exe bksm_v01 1out/
+packer.exe bksm_v02 1out/
+packer.exe bksm_v03 1out/
+packer.exe bksm_v04 1out/
+packer.exe bksm_v05 1out/
+
+packer.exe fstr_v01 1out/
+packer.exe fstr_v02 1out/
+packer.exe fstr_v03 1out/
+packer.exe fstr_v04 1out/
+packer.exe fstr_v05 1out/
+
+packer.exe pfdr_v01 1out/
+packer.exe pfdr_v02 1out/
+packer.exe pfdr_v03 1out/
+packer.exe pfdr_v04 1out/
+packer.exe pfdr_v05 1out/
+
+packer.exe pfpn_v01 1out/
+packer.exe pfpn_v02 1out/
+packer.exe pfpn_v03 1out/
+packer.exe pfpn_v04 1out/
+packer.exe pfpn_v05 1out/
+
+packer.exe lnpl_v01 2clo/
+packer.exe lnpl_v02 2clo/
+packer.exe lnpl_v03 2clo/
+packer.exe lnpl_v04 2clo/
+packer.exe lnpl_v05 2clo/
+packer.exe lnpl_v06 2clo/
+packer.exe lnpl_v07 2clo/
+packer.exe lnpl_v08 2clo/
+packer.exe lnpl_v09 2clo/
+packer.exe lnpl_v10 2clo/
+
+packer.exe mnpl_v01 2clo/
+packer.exe mnpl_v02 2clo/
+packer.exe mnpl_v03 2clo/
+packer.exe mnpl_v04 2clo/
+packer.exe mnpl_v05 2clo/
+packer.exe mnpl_v06 2clo/
+packer.exe mnpl_v07 2clo/
+packer.exe mnpl_v08 2clo/
+packer.exe mnpl_v09 2clo/
+packer.exe mnpl_v10 2clo/
+
+packer.exe gogl_v01 3fac/
+packer.exe gogl_v02 3fac/
+packer.exe gogl_v03 3fac/
+packer.exe gogl_v04 3fac/
+packer.exe gogl_v05 3fac/
+
+packer.exe band_v01 5hat/
+packer.exe band_v02 5hat/
+packer.exe band_v03 5hat/
+packer.exe band_v04 5hat/
+packer.exe band_v05 5hat/
+
+packer.exe hddn_v01 5hat/
+packer.exe hddn_v02 5hat/
+packer.exe hddn_v03 5hat/
+packer.exe hddn_v04 5hat/
+packer.exe hddn_v05 5hat/
+packer.exe hddn_v06 5hat/
+packer.exe hddn_v07 5hat/
+packer.exe hddn_v08 5hat/
+packer.exe hddn_v09 5hat/
+packer.exe hddn_v10 5hat/
+
+packer.exe hdpl_v01 5hat/
+packer.exe hdpl_v02 5hat/
+packer.exe hdpl_v03 5hat/
+packer.exe hdpl_v04 5hat/
+packer.exe hdpl_v05 5hat/
+packer.exe hdpl_v06 5hat/
+packer.exe hdpl_v07 5hat/
+packer.exe hdpl_v08 5hat/
+packer.exe hdpl_v09 5hat/
+packer.exe hdpl_v10 5hat/
+
+packer.exe pfbn_v01 5hat/
+packer.exe pfbn_v02 5hat/
+packer.exe pfbn_v03 5hat/
+packer.exe pfbn_v04 5hat/
+packer.exe pfbn_v05 5hat/
+
+packer.exe pfht_v01 5hat/
+packer.exe pfht_v02 5hat/
+packer.exe pfht_v03 5hat/
+packer.exe pfht_v04 5hat/
+packer.exe pfht_v05 5hat/
+
+packer.exe pnty_v01 5hat/
+packer.exe pnty_v02 5hat/
+packer.exe pnty_v03 5hat/
+packer.exe pnty_v04 5hat/
+packer.exe pnty_v05 5hat/
+
+packer.exe rnht_v01 5hat/
+packer.exe rnht_v02 5hat/
+packer.exe rnht_v03 5hat/
+packer.exe rnht_v04 5hat/
+packer.exe rnht_v05 5hat/
+
+packer.exe lnpl_v01 0bot/
+packer.exe lnpl_v02 0bot/
+packer.exe lnpl_v03 0bot/
+packer.exe lnpl_v04 0bot/
+packer.exe lnpl_v05 0bot/
+packer.exe lnpl_v06 0bot/
+packer.exe lnpl_v07 0bot/
+packer.exe lnpl_v08 0bot/
+packer.exe lnpl_v09 0bot/
+packer.exe lnpl_v10 0bot/
+
+ECHO Texture processing has been completed, please see the output folder.
+PAUSE
---
+++ Mana Collection/Mana Seed Tools (1.12)/Mana Seed Tools/process_textures.bat
@@ -0,0 +1,67 @@
+@ECHO OFF
+
+REM packer.exe demn_v01
+REM packer.exe gbln_v01
+
+packer.exe humn_v00
+
+packer.exe bob1_v00 4har/
+packer.exe bob2_v00 4har/
+packer.exe dap1_v00 4har/
+packer.exe flat_v00 4har/
+packer.exe fro1_v00 4har/
+packer.exe pon1_v00 4har/
+packer.exe spk2_v00 4har/
+
+packer.exe boxr_v01 1out/
+packer.exe undi_v01 1out/
+
+packer.exe farm_v01 6tla/ 1
+packer.exe mine_v01 6tla/ 2
+packer.exe wood_v01 6tla/ 3
+packer.exe roda_v01 6tla/ 4
+packer.exe smth_v01 6tla/ 5
+
+packer.exe bo01_v01 6tla/ 6
+packer.exe bo02_v01 6tla/ 6
+packer.exe bo03_v01 6tla/ 6
+
+packer.exe qv01_v01 7tlb/ 6
+
+packer.exe ax01_v01 6tla/ 7
+packer.exe mc01_v01 6tla/ 7
+packer.exe sw01_v01 6tla/ 7
+packer.exe sw02_v01 6tla/ 7
+
+packer.exe sh01_v01 7tlb/ 7
+packer.exe sh02_v01 7tlb/ 7
+packer.exe sh03_v01 7tlb/ 7
+
+packer.exe hb01_v01 6tla/ 8
+packer.exe sp01_v01 6tla/ 8
+packer.exe sp02_v01 6tla/ 8
+
+packer.exe alch_v01 1out/
+packer.exe angl_v01 1out/
+packer.exe bksm_v01 1out/
+packer.exe fstr_v01 1out/
+packer.exe pfdr_v01 1out/
+packer.exe pfpn_v01 1out/
+
+packer.exe lnpl_v01 2clo/
+packer.exe mnpl_v01 2clo/
+
+packer.exe gogl_v01 3fac/
+
+packer.exe band_v01 5hat/
+packer.exe hddn_v01 5hat/
+packer.exe hdpl_v01 5hat/
+packer.exe pfbn_v01 5hat/
+packer.exe pfht_v01 5hat/
+packer.exe pnty_v01 5hat/
+packer.exe rnht_v01 5hat/
+
+packer.exe lnpl_v01 0bot/
+
+ECHO Texture processing has been completed, please see the output folder.
+PAUSE
